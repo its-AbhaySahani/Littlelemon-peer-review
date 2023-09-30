b89a42478a4f95b7319f2959657ae18708c5f1bc
Abhay Sahani <107034831+its-AbhaySahani@users.noreply.github.com>
2023-10-01 01:48:21 +0530

    Delete littlelemon directory

--- littlelemon/littlelemon/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, re_path, include
-from django.contrib import admin
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include("restaurant.urls")),
-]
